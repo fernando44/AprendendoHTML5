80f40839704dbee2c9a9c053fd97993f2818abc1
Fernando Chiareli Ferreira <fernando.ferreira.chiareli@hotmail.com>
2022-05-19 08:56:59 -0300

    update

--- Desenvolvimento Web Completo 2022/JavaScript/005_Desafio/codigo.js
+++ Desenvolvimento Web Completo 2022/JavaScript/005_Desafio/codigo.js
@@ -2,9 +2,9 @@ var a = 10;
 var b = 20;
 var c = null;
 
-document.write('<p> Valor A = '+a+'<br/>');
-document.write('<p> Valor B = '+b+'<br/>');
-document.write('<p> Valor C = '+c+'<p/>');
+document.write('<p> Valor A = '+a+'</p><br/>');
+document.write('<p> Valor B = '+b+'</p><br/>');
+document.write('<p> Valor C = '+c+'</p>');
 
 document.write('<hr/>');
 
@@ -13,7 +13,7 @@ a = b;
 b = c;
 c = null;
 
-document.write('<p> Valor alterado com sucesso!');
-document.write('<p> Valor A = '+a+'<br/>');
-document.write('<p> Valor B = '+b+'<br/>');
-document.write('<p> Valor C = '+c+'<p/>');
+document.write('<p> Valor alterado com sucesso!</p>');
+document.write('<p> Valor A = '+a+'</p><br/>');
+document.write('<p> Valor B = '+b+'</p><br/>');
+document.write('<p> Valor C = '+c+'</p>');
